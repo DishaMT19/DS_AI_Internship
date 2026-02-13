41d3c5cde47bbade6e6e8a53d089a5c56c0475cd
Disha M T <dishamt08@gmail.com>
2026-02-13 12:19:01 +0530

    Add files via upload

---
+++ src/Day 11/day11_Task1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 13 11:38:49 2026
+
+@author: disha
+"""
+
+# Task 1
+import matplotlib.pyplot as plt
+months = [1, 2, 3, 4, 5]
+revenue = [2000, 4500, 4000, 7500, 9000]
+plt.plot(months, revenue)
+plt.title("Monthly Revenue Growth")
+plt.xlabel("Month")
+plt.ylabel("Revenue in USD")
+plt.show()
